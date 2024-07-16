22b08b6eb8f53092244c16416facdbc9a0ba24dc
khg <hyukgyu3818@naver.com>
2024-07-16 17:21:43 +0900

    계정 상태업데이트 (C,E,F,401error), 계정 사용시간 리셋 service추가 -2

--- src/main/groovy/com/tapacross/sns/crawler/twitter/service/TwitterLoginInfoService.groovy
+++ src/main/groovy/com/tapacross/sns/crawler/twitter/service/TwitterLoginInfoService.groovy
@@ -12,6 +12,14 @@ interface LoginInfoService {
 	def void insertTwitterNewId (TBTwitterAuthToken entity) throws DataAccessException
 	def List<TBTwitterAuthToken> selectStatusFTwitterIdList () throws DataAccessException
 	def void updateTwitterAuthData (TBTwitterAuthToken entity) throws DataAccessException
+	def void updateAuthTokenStatusC (String email) throws DataAccessException
+	def void updateAuthTokenStatusE (String email) throws DataAccessException
+	def void updateAuthTokenStatusF (String email) throws DataAccessException
+	def List<TBTwitterAuthToken> selectIpAuthInfoList (String ip) throws DataAccessException
+	def void updateAuthToken401 (String id) throws DataAccessException
+	def List<TBTwitterAuthToken> select401TwitterId () throws DataAccessException
+	def void updateAuthTokenUseDateReset (TBTwitterAuthToken entity) throws DataAccessException
+	
 }
 
 @Service
@@ -42,6 +50,53 @@ class TwitterLoginInfoService implements LoginInfoService {
 	public void updateTwitterAuthData(TBTwitterAuthToken entity) throws DataAccessException {
 		TwitterDao.updateTwitterAuthData(entity)	
 		println "update in DB : $entity.email"
+	}
+
+	@Override
+	public void updateAuthTokenStatusC(String email) throws DataAccessException {
+		TwitterDao.updateAuthTokenStatusC(email)
+		println "update status C : $email"
+		
+	}
+
+	@Override
+	public void updateAuthTokenStatusE(String email) throws DataAccessException {
+		TwitterDao.updateAuthTokenStatusE(email)
+		println "update status E : $email"	
+	}
+
+	@Override
+	public void updateAuthTokenStatusF(String email) throws DataAccessException {
+		TwitterDao.updateAuthTokenStatusF(email)
+		println "update status F : $email"	
+	}
+
+	@Override
+	public List<TBTwitterAuthToken> selectIpAuthInfoList(String ip) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return TwitterDao.selectIpAuthInfoList(ip)
+		
+	}
+
+	@Override
+	public void updateAuthToken401(String id) throws DataAccessException {
+		TwitterDao.updateAuthToken401(id);
+		println "updateAuthToken 401 error, $id"
+		
+	}
+
+	@Override
+	public List<TBTwitterAuthToken> select401TwitterId() throws DataAccessException {
+		// TODO Auto-generated method stub
+		return TwitterDao.select401TwitterId()
+		
+	}
+
+	@Override
+	public void updateAuthTokenUseDateReset(TBTwitterAuthToken entity) throws DataAccessException {
+		// TODO Auto-generated method stub
+		TwitterDao.updateAuthTokenUseDateReset(entity)
+		
 	}
 
 
